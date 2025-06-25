390effef302079b9cbffc6428f78a7eae6fc0320
leduycuong12345 <leduy_cuong@yahoo.com>
2025-06-25 09:57:21 +0700

    add exception handler which direct user to error page 404 and auto jump to homepage instead of stacktrace error ( only for dev )

---
+++ App/src/main/java/com/cuongsolution/manageproperty/front/web/Exception/GlobalExceptionHandler.java
@@ -0,0 +1,15 @@
+package com.cuongsolution.manageproperty.front.web.Exception;
+
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(Exception.class)
+    public String handleException(Exception ex, Model model) {
+        model.addAttribute("message", ex.getMessage());
+        return "error/404";
+    }
+}
