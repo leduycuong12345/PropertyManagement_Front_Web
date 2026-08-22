2b3c9721bcdc6564533b62b23e9b4b5f2cafaa8a
leduycuong12345 <leduy_cuong@yahoo.com>
2026-08-22 17:32:24 +0700

    f manageTenantPage

--- App/src/main/java/com/cuongsolution/manageproperty/front/web/Controller/ManageTenantController.java
+++ App/src/main/java/com/cuongsolution/manageproperty/front/web/Controller/ManageTenantController.java
@@ -6,6 +6,9 @@ import java.util.List;
 import java.util.UUID;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.oauth2.client.authentication.OAuth2AuthenticationToken;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -32,7 +35,7 @@ public class ManageTenantController {
 	private ManageTenant_ContractService contractService;
 	@Autowired
 	private ManageTenant_TenantService tenantSerive;
-	@GetMapping(value="/quan-ly-khach-thue")
+	/*@GetMapping(value="/quan-ly-khach-thue")
 	public String managePropertyPage( HttpSession session,Model model,Principal principal)  {
 		if(this.landService.getDetailsLandList_ManageNavigation_Production(principal.getName()).isEmpty())//kiem tra xem ng dung da khoi tao Land chua? chua thi khoi tao
 		{
@@ -73,6 +76,60 @@ public class ManageTenantController {
 			return "manage_tenant";
 		}
     }
+	 * */
+	@GetMapping(value="/quan-ly-khach-thue")
+	public String manageTenantPage( HttpSession session,Model model,Authentication authentication)  {
+		
+		if (authentication instanceof OAuth2AuthenticationToken oauthToken) {//oauth login
+	        String oauthUsername=authentication.getName();
+			return extracted_manageTenantPage(session, model, oauthUsername);
+		} else {
+	        // local/form login
+	        UserDetails userDetails = (UserDetails) authentication.getPrincipal();
+	        String username=userDetails.getUsername();
+			return extracted_manageTenantPage(session, model, username);
+	    }
+    }
+	private String extracted_manageTenantPage(HttpSession session, Model model,String username) {
+		if(this.landService.getDetailsLandList_ManageNavigation_Production(username).isEmpty())//kiem tra xem ng dung da khoi tao Land chua? chua thi khoi tao
+		{
+			model.addAttribute("newLand", new ManageNavigation_FastCreateLandDTO());//for create land func
+			return "new_user";
+		}
+		else
+		{
+			UUID selectedLandID=(UUID) session.getAttribute("selectedLandID");
+			if(selectedLandID !=null)//neu da chon land
+			{
+				List<ManageNavigation_FastCreateLandDTO> landList=this.landService.getDetailsLandList_ManageNavigation_Production(username);
+				model.addAttribute("landList",landList);//for land list/delete/update func
+				model.addAttribute("newLand", new ManageNavigation_FastCreateLandDTO());//for create land func
+				
+				for(ManageNavigation_FastCreateLandDTO land:landList)
+				{
+					if(land.getLandID()==selectedLandID)
+					{
+						model.addAttribute("selectedLandID",land.getLandID());//to create-property belong to land
+						model.addAttribute("selectedLand",land );//to display selected-land-name at layout-sidebar
+						model.addAttribute("propertyList",this.propertyService.getPropertyListIncludedTenantsByLandId(land.getLandID()));//tenant-list function
+						model.addAttribute("editTenant",new ManageTenant_EditTenant_TenantDTO());//edit tenant function
+					}
+				}
+			}
+			else//neu chua chon land
+			{
+				List<ManageNavigation_FastCreateLandDTO> landList=this.landService.getDetailsLandList_ManageNavigation_Production(username);//for land list/delete/update func
+				model.addAttribute("landList",landList);//for land list/delete/update func
+				model.addAttribute("newLand", new ManageNavigation_FastCreateLandDTO());//for create land func
+				model.addAttribute("selectedLandID",landList.get(0).getLandID());//to create-property belong to land
+				model.addAttribute("selectedLand",landList.get(0));//to display selected-land-name at layout-sidebar
+				model.addAttribute("editTenant",new ManageTenant_EditTenant_TenantDTO());//edit tenant function
+
+				model.addAttribute("propertyList",this.propertyService.getPropertyListIncludedTenantsByLandId(landList.get(0).getLandID()));//tenant-list function
+			}
+			return "manage_tenant";
+		}
+	}
 	@PostMapping(value="/quan-ly-khach-thue")
 	public String selectLandtoManage(@RequestParam("selectedLandID") Long selectedLandID, HttpSession session)  {
 		session.setAttribute("selectedLandID", selectedLandID);
