029ee1289d4149d213251467d5ddcc22062b99f8
leduycuong12345 <leduy_cuong@yahoo.com>
2026-09-21 14:32:03 +0700

    change to vn_format number

--- App/src/main/resources/static/js/manage_expanse.js
+++ App/src/main/resources/static/js/manage_expanse.js
@@ -13,9 +13,51 @@ function selectAllCheckbox()
 	  $(this).closest('form').children('div').children('input[name="selectedPropertyServiceIDList"]').prop("checked",isChecked);
   	});
 }
+function safeLoadFloatValue(containerValue)
+{
+	return parseFloat(containerValue.replace(/,/g, ''));
+}
+function formatExpanseCostWithComma()
+{
+	// Iterate through each <li> with name "expanseCost"
+    $('p[name="expanseCost"]').each(function() {
+	      var content = $(this).text();
+	      
+	      // Convert the content to a number
+	      var number = safeLoadFloatValue(content);
+	
+	      // Format the number with commas every 3 digits
+	      var formattedNumber = number.toLocaleString();
+	
+	      // Replace the content of the <p> element with the formatted number
+	      $(this).text(formattedNumber);
+     
+    });
+    
+}
+function formatAppliedPropertyTotalWithComma()
+{
+	// Iterate through each <li> with name "appliedPropertyTotal"
+    $('p[name="appliedPropertyTotal"]').each(function() {
+	      var content = $(this).text();
+	      
+	      // Convert the content to a number
+	      var number = safeLoadFloatValue(content);
+	
+	      // Format the number with commas every 3 digits
+	      var formattedNumber = number.toLocaleString();
+	
+	      // Replace the content of the <p> element with the formatted number
+	      $(this).text(formattedNumber);
+     
+    });
+    
+}
 $(document).ready(function(){
 
 	//select all at edit-expanse function
 	selectAllCheckbox();
     
+    formatExpanseCostWithComma();
+    formatAppliedPropertyTotalWithComma();
 });
