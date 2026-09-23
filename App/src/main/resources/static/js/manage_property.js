26c88da952941b5bb116118dfd6d51e22aecee80
leduycuong12345 <leduy_cuong@yahoo.com>
2026-09-23 16:07:42 +0700

    auto_format_number at worksheetRentalPrice and depositAmount at createContact at manageproperty.html

--- App/src/main/resources/static/js/manage_property.js
+++ App/src/main/resources/static/js/manage_property.js
@@ -766,6 +766,136 @@ function searchWorksheet_createFastOrderList()
 	    });
   	});
 }
+
+//UI update number format of depositAmount in create_contract
+function formatNumberTextarea_depositAmount_CreateContact_FocusOn_BlurOut()
+{
+	 // On focus: convert UI display from "1.500.500" -> "1500500"
+    $('textarea.editable_depositAmount_textarea').on('focus', function() {
+        var text = $(this).val().trim(); 
+        var raw = text.replace(/\./g, ""); // remove all dot 
+        $(this).text(raw);
+        $(this).val(raw);
+    });
+
+    // On blur: convert display back "1500500,5" -> "1.500.500,5"
+    $('textarea.editable_depositAmount_textarea').on('blur', function() {
+    	var content =  $(this).closest('div').children('input[name="depositAmount"]').val() ;
+        // Convert the content to a number
+	    var number = safeLoadFloatValue(content);
+	    // Format the number with commas every 3 digits
+	    var formattedNumber = number.toLocaleString();
+	    //parse to textarea display UI
+	    $(this).val(formattedNumber);
+	    $(this).text(formattedNumber);
+    });
+}
+function updateValueToHiddenInputField_editableTextarea_depositAmount_CreateContact()
+{
+	$(".editable_depositAmount_textarea").change(function (e) {
+           var content=$(this).val();
+           
+           //update value of hidden input after edit at textarea to submit post form if needed be.
+           $(this).closest('div').children('input[name="depositAmount"]').val(content);
+     });
+}
+function numberInputrOnly_depositAmount_CreateContact_editableTextarea()
+{
+	$(".editable_depositAmount_textarea").keypress(function (e) {
+	    var allowedCharacters = []; // Mã ký tự của dấu d0t và các số từ 0 đến 9 
+	    //"." (dot) = 46"," (comma) = 44
+	    var keyCode = e.which;
+	    
+	    if (!(allowedCharacters.includes(keyCode) || (keyCode >= 48 && keyCode <= 57))) {
+	        e.preventDefault();
+	    }
+	});
+}
+function formatNumber_depositAmount_CreateContact_editableTextarea_firstTimeRender()
+{
+	$('.editable_depositAmount_textarea').each(function() {
+        var content = $(this).val();
+        // Convert the content to a number
+	    var number = safeLoadFloatValue(content);
+	    // Format the number with commas every 3 digits
+	    var formattedNumber = number.toLocaleString();
+	    // Replace the content of the <p> element with the formatted number
+	    $(this).text(formattedNumber);
+    });
+}
+function updateNumberFormat_depositAmount_CreateContact()
+{
+	formatNumberTextarea_depositAmount_CreateContact_FocusOn_BlurOut();
+	updateValueToHiddenInputField_editableTextarea_depositAmount_CreateContact();
+	numberInputrOnly_depositAmount_CreateContact_editableTextarea();
+	formatNumber_depositAmount_CreateContact_editableTextarea_firstTimeRender();
+}
+//UI update number format of depositAmount in create_contract ending
+
+//UI update number format of worksheetRentalPrice in create_contract
+function formatNumberTextarea_worksheetRentalPrice_CreateContact_FocusOn_BlurOut()
+{
+	 // On focus: convert UI display from "1.500.500" -> "1500500"
+    $('textarea.editable_worksheetRentalPrice_textarea').on('focus', function() {
+        var text = $(this).val().trim(); 
+        var raw = text.replace(/\./g, ""); // remove all dot 
+        $(this).text(raw);
+        $(this).val(raw);
+    });
+
+    // On blur: convert display back "1500500,5" -> "1.500.500,5"
+    $('textarea.editable_worksheetRentalPrice_textarea').on('blur', function() {
+    	var content =  $(this).closest('div').children('input[name="worksheetRentalPrice"]').val() ;
+        // Convert the content to a number
+	    var number = safeLoadFloatValue(content);
+	    // Format the number with commas every 3 digits
+	    var formattedNumber = number.toLocaleString();
+	    //parse to textarea display UI
+	    $(this).val(formattedNumber);
+	    $(this).text(formattedNumber);
+    });
+}
+function updateValueToHiddenInputField_editableTextarea_worksheetRentalPrice_CreateContact()
+{
+	$(".editable_worksheetRentalPrice_textarea").change(function (e) {
+           var content=$(this).val();
+           
+           //update value of hidden input after edit at textarea to submit post form if needed be.
+           $(this).closest('div').children('input[name="worksheetRentalPrice"]').val(content);
+     });
+}
+function numberInputrOnly_worksheetRentalPrice_CreateContact_editableTextarea()
+{
+	$(".editable_worksheetRentalPrice_textarea").keypress(function (e) {
+	    var allowedCharacters = []; // Mã ký tự của dấu d0t và các số từ 0 đến 9 
+	    //"." (dot) = 46"," (comma) = 44
+	    var keyCode = e.which;
+	    
+	    if (!(allowedCharacters.includes(keyCode) || (keyCode >= 48 && keyCode <= 57))) {
+	        e.preventDefault();
+	    }
+	});
+}
+function formatNumber_worksheetRentalPrice_CreateContact_editableTextarea_firstTimeRender()
+{
+	$('.editable_worksheetRentalPrice_textarea').each(function() {
+        var content = $(this).val();
+        // Convert the content to a number
+	    var number = safeLoadFloatValue(content);
+	    // Format the number with commas every 3 digits
+	    var formattedNumber = number.toLocaleString();
+	    // Replace the content of the <p> element with the formatted number
+	    $(this).text(formattedNumber);
+    });
+}
+function updateNumberFormat_worksheetRentalPrice_CreateContact()
+{
+	formatNumberTextarea_worksheetRentalPrice_CreateContact_FocusOn_BlurOut();
+	updateValueToHiddenInputField_editableTextarea_worksheetRentalPrice_CreateContact();
+	numberInputrOnly_worksheetRentalPrice_CreateContact_editableTextarea();
+	formatNumber_worksheetRentalPrice_CreateContact_editableTextarea_firstTimeRender();
+}
+//UI update number format of worksheetRentalPrice in create_contract ending
 $(document).ready(function(){
 
 	//create-worksheet
@@ -805,7 +935,10 @@ $(document).ready(function(){
 		createFastOrderList();
     });
     
-    
+    //update numberformat 
+    updateNumberFormat_depositAmount_CreateContact();
+    updateNumberFormat_worksheetRentalPrice_CreateContact();
+    //ending update numberformat
     
 });
 
