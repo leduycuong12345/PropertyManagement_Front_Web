6d8b144d3b4671036edb00c4c71b1aa57a6c2275
leduycuong12345 <leduy_cuong@yahoo.com>
2026-09-24 12:06:34 +0700

    auto formatNumber propertyRentalPrice at add_property_to_land in manageproperty.html

--- App/src/main/resources/static/js/manage_property.js
+++ App/src/main/resources/static/js/manage_property.js
@@ -1064,6 +1064,71 @@ function updateNumberFormat_depositAmount_CreateOrderForDeposit()
 } 
 //UI update number format of depositAmount cost in create_order_for_deposit ending 
 
+//UI update number format of propertyRentalPrice in create_property 
+function formatNumberTextarea_propertyRentalPrice_CreateProperty_FocusOn_BlurOut()
+{
+	 // On focus: convert UI display from "1.500.500" -> "1500500"
+    $('textarea.editable_propertyRentalPrice_CreateProperty_textarea').on('focus', function() {
+        var text = $(this).val().trim(); 
+        var raw = text.replace(/\./g, ""); // remove all dot 
+        $(this).text(raw);
+        $(this).val(raw);
+    });
+
+    // On blur: convert display back "1500500,5" -> "1.500.500,5"
+    $('textarea.editable_propertyRentalPrice_CreateProperty_textarea').on('blur', function() {
+    	var content =  $(this).closest('div').children('input[name="propertyRentalPrice"]').val() ;
+        // Convert the content to a number
+	    var number = safeLoadFloatValue(content);
+	    // Format the number with commas every 3 digits
+	    var formattedNumber = number.toLocaleString();
+	    //parse to textarea display UI
+	    $(this).val(formattedNumber);
+	    $(this).text(formattedNumber);
+    });
+}
+function updateValueToHiddenInputField_editableTextarea_propertyRentalPrice_CreateProperty()
+{
+	$(".editable_propertyRentalPrice_CreateProperty_textarea").change(function (e) {
+           var content=$(this).val();
+           
+           //update value of hidden input after edit at textarea to submit post form if needed be.
+           $(this).closest('div').children('input[name="propertyRentalPrice"]').val(content);
+     });
+}
+function numberInputrOnly_propertyRentalPrice_CreateProperty_editableTextarea()
+{
+	$(".editable_propertyRentalPrice_CreateProperty_textarea").keypress(function (e) {
+	    var allowedCharacters = []; // Mã ký tự của dấu d0t và các số từ 0 đến 9 
+	    //"." (dot) = 46"," (comma) = 44
+	    var keyCode = e.which;
+	    
+	    if (!(allowedCharacters.includes(keyCode) || (keyCode >= 48 && keyCode <= 57))) {
+	        e.preventDefault();
+	    }
+	});
+}
+function formatNumber_propertyRentalPrice_CreateProperty_editableTextarea_firstTimeRender()
+{
+	$('.editable_propertyRentalPrice_CreateProperty_textarea').each(function() {
+        var content = $(this).val();
+        // Convert the content to a number
+	    var number = safeLoadFloatValue(content);
+	    // Format the number with commas every 3 digits
+	    var formattedNumber = number.toLocaleString();
+	    // Replace the content of the <p> element with the formatted number
+	    $(this).text(formattedNumber);
+    });
+}
+function updateNumberFormat_propertyRentalPrice_CreateProperty()
+{
+	formatNumberTextarea_propertyRentalPrice_CreateProperty_FocusOn_BlurOut();
+	formatNumber_propertyRentalPrice_CreateProperty_editableTextarea_firstTimeRender();
+	numberInputrOnly_propertyRentalPrice_CreateProperty_editableTextarea();
+	updateValueToHiddenInputField_editableTextarea_propertyRentalPrice_CreateProperty();
+} 
+//UI update number format of propertyRentalPrice in create_property ending
+
 $(document).ready(function(){
 
 	//create-worksheet
@@ -1104,6 +1169,7 @@ $(document).ready(function(){
     updateNumberFormat_unexpectedExpanseCost_CreateOrder();
     updateNumberFormat_depositAmount_CreateOrderForDeposit();
     updateNumberFormat_EditProperty();
+    updateNumberFormat_propertyRentalPrice_CreateProperty();
     //ending update numberformat
     
 });
