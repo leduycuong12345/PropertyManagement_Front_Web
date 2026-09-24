84899f9d73666816644dbdaa6198f570bf08ae96
leduycuong12345 <leduy_cuong@yahoo.com>
2026-09-24 09:34:13 +0700

    auto_format_number depositAmount in createOrderForDeposit at manage_property.html

--- App/src/main/resources/static/js/manage_property.js
+++ App/src/main/resources/static/js/manage_property.js
@@ -962,6 +962,70 @@ function updateNumberFormat_unexpectedExpanseCost_CreateOrder()
 } 
 //UI update number format of unexpected_expanse cost in create_order ending 
 
+//UI update number format of depositAmount cost in create_order_for_deposit
+function formatNumberTextarea_depositAmount_CreateOrderForDeposit_FocusOn_BlurOut()
+{
+	 // On focus: convert UI display from "1.500.500" -> "1500500"
+    $('textarea.editable_depositAmount_createOrderForDeposit_textarea').on('focus', function() {
+        var text = $(this).val().trim(); 
+        var raw = text.replace(/\./g, ""); // remove all dot 
+        $(this).text(raw);
+        $(this).val(raw);
+    });
+
+    // On blur: convert display back "1500500,5" -> "1.500.500,5"
+    $('textarea.editable_depositAmount_createOrderForDeposit_textarea').on('blur', function() {
+    	var content =  $(this).closest('div').children('input[name="depositAmount"]').val() ;
+        // Convert the content to a number
+	    var number = safeLoadFloatValue(content);
+	    // Format the number with commas every 3 digits
+	    var formattedNumber = number.toLocaleString();
+	    //parse to textarea display UI
+	    $(this).val(formattedNumber);
+	    $(this).text(formattedNumber);
+    });
+}
+function updateValueToHiddenInputField_editableTextarea_depositAmount_CreateOrderForDeposit()
+{
+	$(".editable_depositAmount_createOrderForDeposit_textarea").change(function (e) {
+           var content=$(this).val();
+           
+           //update value of hidden input after edit at textarea to submit post form if needed be.
+           $(this).closest('div').children('input[name="depositAmount"]').val(content);
+     });
+}
+function numberInputrOnly_depositAmount_CreateOrderForDeposit_editableTextarea()
+{
+	$(".editable_depositAmount_createOrderForDeposit_textarea").keypress(function (e) {
+	    var allowedCharacters = []; // Mã ký tự của dấu d0t và các số từ 0 đến 9 
+	    //"." (dot) = 46"," (comma) = 44
+	    var keyCode = e.which;
+	    
+	    if (!(allowedCharacters.includes(keyCode) || (keyCode >= 48 && keyCode <= 57))) {
+	        e.preventDefault();
+	    }
+	});
+}
+function formatNumber_depositAmount_CreateOrderForDeposit_editableTextarea_firstTimeRender()
+{
+	$('.editable_depositAmount_createOrderForDeposit_textarea').each(function() {
+        var content = $(this).val();
+        // Convert the content to a number
+	    var number = safeLoadFloatValue(content);
+	    // Format the number with commas every 3 digits
+	    var formattedNumber = number.toLocaleString();
+	    // Replace the content of the <p> element with the formatted number
+	    $(this).text(formattedNumber);
+    });
+}
+function updateNumberFormat_depositAmount_CreateOrderForDeposit()
+{
+	formatNumberTextarea_depositAmount_CreateOrderForDeposit_FocusOn_BlurOut();
+	updateValueToHiddenInputField_editableTextarea_depositAmount_CreateOrderForDeposit();
+	numberInputrOnly_depositAmount_CreateOrderForDeposit_editableTextarea();
+	formatNumber_depositAmount_CreateOrderForDeposit_editableTextarea_firstTimeRender();
+} 
+//UI update number format of depositAmount cost in create_order_for_deposit ending 
 $(document).ready(function(){
 
 	//create-worksheet
@@ -1005,6 +1069,7 @@ $(document).ready(function(){
     updateNumberFormat_depositAmount_CreateContact();
     updateNumberFormat_worksheetRentalPrice_CreateContact();
     updateNumberFormat_unexpectedExpanseCost_CreateOrder();
+    updateNumberFormat_depositAmount_CreateOrderForDeposit();
     //ending update numberformat
     
 });
